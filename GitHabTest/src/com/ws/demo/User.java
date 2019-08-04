fd27808ce83388d6bbb09cde7061df0e6c9908f1
wangsheng82 <36830820@qq.com>
2019-08-04 12:13:07 +0800

    上传用户DTO

---
+++ GitHabTest/src/com/ws/demo/User.java
@@ -0,0 +1,15 @@
+package com.ws.demo;
+
+public class User {
+	private String userName;
+	
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
